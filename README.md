f0e1dab888b1cbf95132466cec7501edf52c8acf
Fortzy05 <39365492+Fortzy05@users.noreply.github.com>
2022-09-30 13:43:46 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Translator
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-cfhncy)
---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./style.css";
+
+ const App = () => {
+  return (
+    <div className="ui container">
+      
+      <p></p>
+    </div>
+  );
+}
+export default App;
